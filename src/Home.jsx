90f9535de0748cfe7aae995d6d8cb7a627c8e4e4
kaif <kaifbscit101@gmail.com>
2024-12-29 20:20:54 +0530

    title change

--- src/Home.jsx
+++ src/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
       image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
     },
     {
-      title: "Smart Watchhhhhhhhhh", 
+      title: "Smarttt watchhh", 
       description: "Feature-rich smartwatch with health tracking",
       price: 199.99,
       image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30"
